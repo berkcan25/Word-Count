aa2293b037534279ebb4e1daab6f34dec80bd562
berkcan25 <bctunctan@gmail.com>
2023-12-08 14:30:31 +0100

    Added character count

--- Word Count Checker.py
+++ Word Count Checker.py
@@ -5,7 +5,10 @@ def word_count(file):
     average_word_length = 0
     word_count = 0
     word_length = 0
+    char_count = 0
     for char in text:
+        if re.match("[^\s]", char):
+                char_count += 1
         if re.match("[\w']", char):
             word_length += 1
         else:
@@ -13,8 +16,9 @@ def word_count(file):
                 word_count += 1
                 average_word_length = average_word_length+(word_length-average_word_length)/word_count
             word_length = 0
-    print(word_count)
-    print(round(average_word_length, 2))
+    print("Word count:", word_count)
+    print("Average word length:", round(average_word_length, 2))
+    print("Character count:", char_count)
 if __name__ == "__main__":
     file = input("Please enter the intended file path: ")
     word_count(file)
