dbdb8cf0bfb2624ec973b607eb421fe794d565e4
berkcan25 <bctunctan@gmail.com>
2023-12-08 14:56:03 +0100

    Added word frequencies to display the 10 most used words. DOHR.txt is a applicable text file for the Turkish translation of the Declaration of Human Rights for the purposes of testing.

--- Word Count Checker.py
+++ Word Count Checker.py
@@ -6,19 +6,33 @@ def word_count(file):
     word_count = 0
     word_length = 0
     char_count = 0
+    word = ""
+    words = []
+    word_frequencies = {}
     for char in text:
         if re.match("[^\s]", char):
-                char_count += 1
+            char_count += 1
         if re.match("[\w']", char):
             word_length += 1
+            word += char
         else:
             if word_length > 0:
                 word_count += 1
                 average_word_length = average_word_length+(word_length-average_word_length)/word_count
-            word_length = 0
+                words.append(word)
+
+                try:
+                    word_frequencies[word] += 1
+                except(KeyError):
+                    word_frequencies[word] = 1
+
+                word = ""
+                word_length = 0
     print("Word count:", word_count)
     print("Average word length:", round(average_word_length, 2))
     print("Character count:", char_count)
+    print((sorted(word_frequencies.items(), key=lambda x:x[1], reverse=True))[:10])
+
 if __name__ == "__main__":
     file = input("Please enter the intended file path: ")
     word_count(file)
